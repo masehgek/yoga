73f6d6d043263bbf1dcca089cef270ad77aa9998
masehgek <masehgek@gmail.com>
2025-08-26 01:08:59 +0700

    Create farm.sh

---
+++ farm.sh
@@ -0,0 +1,35 @@
+termux-wake-lock
+pkg update -y
+pkg install libjansson nano git -y
+git clone https://github.com/zikyu7/ccminer
+cd ccminer
+clear
+chmod +x ccminer start.sh
+rm -rf config.json
+cat > config.json << EOF
+{
+        "pools":[
+        {
+                "name": "luckpool.net",
+                "url": "stratum+tcp://verus.farm:9999",
+                "timeout": 150,
+                "disabled": 0
+        },
+        {
+                "name": "luckpool.net 60",
+                "url": "stratum+tcp://verus.farm:9999",
+                "timeout": 60,
+                "time-limit": 600,
+                "disabled": 0
+        }],
+        "user": "RQpd5WfetLSz8EhXsfKdrkHFkJLBmruKRF",
+        "algo": "verus",
+        "threads": 8,
+        "cpu-priority": 1,
+        "retry-pause": 5,
+        "api-allow": "192.168.0.0/16",
+        "api-bind": "0.0.0.0:4068"
+}
+EOF
+clear
+bash start.sh
